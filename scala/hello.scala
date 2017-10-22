c71b1b5518718aa8a105df8164854a30e88e8f23
Mahendra Yadav <mahendra@azrisolutions.com>
2017-10-22 16:18:50 +0530

    Add scala code

---
+++ scala/hello.scala
@@ -0,0 +1,11 @@
+// LANGUAGE: Scala
+// AUTHOR: Mahendra Yadav
+object Hello {
+  def main(args: Array[String]){
+    println("Hello, World!")
+  }
+}
+
+// Execution
+// $ scalac hello.scala
+// $ scala Hello
