10770c6c779a832fc345f252e76db96e7764da87
chinmayshah99 <chinmayshah3899@gmail.com>
2017-10-22 11:47:22 +0530

    Added cpp

---
+++ cpp/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// using namespace std; //refer this for more info: http://en.cppreference.com/w/cpp/utility/functional/function
+
+int main(int argc, char const *argv[])
+{
+	std::cout<<"Hello, world"<<std::endl;
+	return 0;
+}
