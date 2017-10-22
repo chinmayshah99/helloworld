26b499af93c6f51c32c9d9a66588491e153eb6b7
Nick Coffey <ncoffey88@outlook.com>
2017-10-22 16:11:08 -0400

    Add files via upload

--- powershell/hello.ps1
+++ powershell/hello.ps1
@@ -1,2 +1,9 @@
+#LANGUAGE: Powershell
+#AUTHOR: Nick Coffey
+
 Write-Host "Hello World!"
 pause
+
+#Execution:
+#Insert your path where shown and run:
+#powershell -executionpolicy bypass -File C:\my_path\powershell\hello.ps1
